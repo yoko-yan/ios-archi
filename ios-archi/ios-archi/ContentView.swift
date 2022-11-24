1197cadb459e1007f1208f03fab4d993e6ceae62
yoko-yan <459098+yoko-yan@users.noreply.github.com>
2022-11-24 10:57:03 +0900

    初期コミット
    Signed-off-by: yoko-yan <>

---
+++ ios-archi/ios-archi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-archi
+//
+//  Created by yokoda.takayuki on 2022/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-archi/ios-archi/ios_archiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_archiApp.swift
+//  ios-archi
+//
+//  Created by yokoda.takayuki on 2022/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_archiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
