8de84dd1d45f7060a61002d5f5553333eb407548
yoko-yan <459098+yoko-yan@users.noreply.github.com>
2023-09-06 12:38:05 +0900

    正規表現の文字列抽出ロジックを修正

--- Sources/AppFeature/Domain/GetCoordinatesUseCase.swift
+++ Sources/AppFeature/Domain/GetCoordinatesUseCase.swift
@@ -16,25 +16,16 @@ struct GetCoordinatesUseCaseImpl {
         let texts = try await recognizeTextRepository.get(image: image)
         // 座標が分割して認識されてしまったケースを考慮し、読み取れた文字を１つの文字列にして、座標の形式にマッチするものをCoordinatesにする
         let text = texts.joined(separator: " ")
-        let pattern = #"(-?[0-9]{1,4},[\s]?-?[0-9]{1,4},[\s]?-?[0-9]{1,4})"#
+        let pattern = #"(-?[0-9]{1,4})[,|.][\s]?(-?[0-9]{1,4})[,|.][\s]?(-?[0-9]{1,4})"#
         // swiftlint:disable:next force_try
         let regex = try! NSRegularExpression(pattern: pattern, options: [.allowCommentsAndWhitespace])
-        let matches = regex.matches(in: text, options: [], range: NSRange(0..<text.count))
-        if let match = matches.first {
-            let range = match.range(at: 1)
-            if let range = Range(range, in: text) {
-                let coordinates = text[range]
-                print(coordinates)
-                let arr = coordinates.components(separatedBy: ",")
-                if arr.count >= 3,
-                   let x = Int(arr[0].trimmingCharacters(in: .whitespaces)),
-                   let y = Int(arr[1].trimmingCharacters(in: .whitespaces)),
-                   let z = Int(arr[2].trimmingCharacters(in: .whitespaces))
-                {
-                    return Coordinates(x: x, y: y, z: z)
-                }
-                return nil
-            }
+        let textNS = NSString(string: text)
+        if let matche = regex.firstMatch(in: text, options: [], range: NSRange(0..<text.count)),
+           let x = Int(textNS.substring(with: matche.range(at: 1))),
+           let y = Int(textNS.substring(with: matche.range(at: 2))),
+           let z = Int(textNS.substring(with: matche.range(at: 3)))
+        {
+            return Coordinates(x: x, y: y, z: z)
         }
         return nil
     }
