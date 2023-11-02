b6d77d7605f01a346a51e1d2d97d7ddcb2292da2
yoko-yan <459098+yoko-yan@users.noreply.github.com>
2023-11-03 03:52:31 +0900

    add AppInfoView

---
+++ Sources/AppFeature/UI/AppInfoView.swift
@@ -0,0 +1,51 @@
+//
+//  Created by yoko-yan on 2023/11/03
+//
+
+import SwiftUI
+
+struct AppInfoView: View {
+    @Environment(\.dismiss) var dismiss
+
+    var body: some View {
+        NavigationStack {
+            VStack {
+                let (version, build) = vesionInfo
+                HStack {
+                    Text("Version")
+                    Spacer()
+                    Text(version)
+                }
+                .padding()
+
+                HStack {
+                    Text("Build Version")
+                    Spacer()
+                    Text(build)
+                }
+                .padding()
+
+                Spacer()
+            }
+            .navigationBarTitle("", displayMode: .inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: { dismiss() }) {
+                        Image(systemName: "xmark")
+                    }
+                }
+            }
+            .toolbarBackground(.visible, for: .navigationBar)
+        }
+    }
+
+    private var vesionInfo: (String, String) {
+        let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String // swiftlint:disable:this force_cast
+        let build = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as! String // swiftlint:disable:this force_cast
+        return (version, build)
+    }
+}
+
+#Preview {
+    AppInfoView()
+}
--- Sources/AppFeature/UI/Home/HomeView.swift
+++ Sources/AppFeature/UI/Home/HomeView.swift
@@ -6,9 +6,21 @@ import Core
 import SwiftUI
 
 struct HomeView: View {
+    @State private var isShowAppInfoView = false
+
     var body: some View {
         NavigationStack {
             TimeLineView()
+                .navigationBarItems(
+                    trailing: Button(action: {
+                        isShowAppInfoView.toggle()
+                    }) {
+                        Image(systemName: "info.circle.fill")
+                    }
+                )
+        }
+        .sheet(isPresented: $isShowAppInfoView) {
+            AppInfoView()
         }
         .analyticsScreen(name: "HomeView", class: String(describing: type(of: self)))
     }
