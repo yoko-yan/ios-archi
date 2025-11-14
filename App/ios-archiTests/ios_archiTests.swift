912f79497e5b519adbab9a46f5aa98db6027763c
yoko-yan <459098+yoko-yan@users.noreply.github.com>
2025-11-15 03:12:23 +0900

    ユニットテスト追加

---
+++ App/ios-archiTests/ios_archiTests.swift
@@ -0,0 +1,14 @@
+//
+//  Created by yoko-yan on 2025/11/15
+//
+    
+
+import Testing
+
+struct ios_archiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
