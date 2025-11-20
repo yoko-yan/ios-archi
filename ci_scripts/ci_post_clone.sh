797f158ae9c96f495408dc5af7480b9e62655ac5
yoko-yan <459098+yoko-yan@users.noreply.github.com>
2025-11-21 04:20:32 +0900

    make sourcery

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+make sourcery
+
 echo "$XCCONFIG" | base64 --decode > ../App/ios-archi/XCConfigs/Local.xcconfig
 echo "$GOOGLE_SERVICE_INFO" | base64 --decode > ../App/ios-archi/Configs/GoogleService-Info-Release.plist
 echo "$GOOGLE_SERVICE_INFO_DEBUG" | base64 --decode > ../App/ios-archi/Configs/GoogleService-Info-Debug.plist
